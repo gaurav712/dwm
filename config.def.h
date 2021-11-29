acae284ddbd1da0fbf2cba9eb06cc627b2a9ed37
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2021-11-29 21:48:27 +0530

    add scratchpad

--- config.def.h
+++ config.def.h
@@ -61,6 +61,9 @@ static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run"};
 static const char *termcmd[] = {"st", NULL};
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = {
+    "st", "-t", scratchpadname, "-g", "120x34", "nvim", NULL};
 static const char *firefoxcmd[] = {"firefox", NULL};
 // static const char *musiccmd[] = {"st", "-c", "cmus", "-e", "cmus", NULL};
 static const char *shotcmd[] = {"shot", NULL};
@@ -81,6 +84,7 @@ static Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY, XK_y, togglescratch, {.v = scratchpadcmd}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
