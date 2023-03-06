50bab9c6d4c15ec4ac431ff9d129945c6d01443d
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-03-06 09:38:18 +0530

    change font to Plex Mono

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 15;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font:size=11" };
-static const char dmenufont[]       = "FiraCode Nerd Font:size=11";
+static const char *fonts[]          = { "BlexMono Nerd Font:size=11" };
+static const char dmenufont[]       = "BlexMono Nerd Font:size=11";
 static const char col_black[]       = "#1d2021";
 static const char col_gray[]        = "#444444";
 static const char col_white[]       = "#ebdbb2";
